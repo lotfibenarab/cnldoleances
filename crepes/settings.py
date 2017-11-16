0c44282ed83d57792ffab92a7d71239929398bf4
benarab <lotfibenarab13@gmail.com>
2017-11-16 10:32:16 +0100

    Added files

--- crepes/settings.py
+++ crepes/settings.py
@@ -96,6 +96,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -107,4 +111,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
