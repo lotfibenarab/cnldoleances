acee37dc8039787fa2e90843ea53ef89973f7041
benarab <lotfibenarab13@gmail.com>
2017-11-16 10:37:23 +0100

    Added files

--- crepes/urls.py
+++ crepes/urls.py
@@ -26,6 +26,3 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
 ]
-
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
